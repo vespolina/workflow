6a5f1700674a5e47e5b434b70f99294cefbafa82
Richard Shank <develop@zestic.com>
2013-09-21 13:32:25 -0700

    add location to token

--- lib/Vespolina/Workflow/Token.php
+++ lib/Vespolina/Workflow/Token.php
@@ -4,7 +4,30 @@ namespace Vespolina\Workflow;
 
 class Token implements TokenInterface
 {
+    protected $location;
     protected $object;
+
+    /**
+     * Set the location
+     *
+     * @param NodeInterface $location
+     */
+    public function setLocation(NodeInterface $location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Return the location
+     *
+     * @return NodeInterface
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
 
     /**
      * Set the object
--- lib/Vespolina/Workflow/TokenInterface.php
+++ lib/Vespolina/Workflow/TokenInterface.php
@@ -5,6 +5,20 @@ namespace Vespolina\Workflow;
 interface TokenInterface
 {
     /**
+     * Set the location
+     *
+     * @param NodeInterface $location
+     */
+    public function setLocation(NodeInterface $location);
+
+    /**
+     * Return the location
+     *
+     * @return NodeInterface
+     */
+    public function getLocation();
+
+    /**
      * Set the object
      *
      * @param mixed $object

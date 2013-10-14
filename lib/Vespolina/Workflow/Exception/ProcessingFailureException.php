488717a44d3ed2e124f32dd97c0250d00859871c
Richard Shank <develop@zestic.com>
2013-10-13 22:35:37 -0700

    add ProcessingFailureException

---
+++ lib/Vespolina/Workflow/Exception/ProcessingFailureException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vespolina\Workflow\Exception;
+
+/**
+ * An exception when a tokenable class processing has a failure
+ */
+class ProcessingFailureException extends \Exception
+{
+
+} 

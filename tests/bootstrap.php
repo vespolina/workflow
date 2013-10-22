acae47a39497b0f41a49f342554d707fb126d5cb
Luis Cordova <cordoval@gmail.com>
2013-10-21 21:19:13 -0500

    add license headers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * (c) 2013 - ∞ Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 $loader = @include __DIR__ . '/../vendor/autoload.php';
 
 if (!$loader) {
